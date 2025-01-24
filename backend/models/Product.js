184e712985d4119c2282190334fd44fb9449be75
bisru1984 <fikertetadesse1403@gmail.com>
2025-01-24 10:38:28 +0300

    Update Product.js

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,29 +1,59 @@
 const mongoose = require('mongoose');
 
+
+
 const productSchema = new mongoose.Schema({
+
     name: {
+
         type: String,
+
         required: true
+
     },
+
     description: {
+
         type: String,
+
         required: true
+
     },
+
     price: {
+
         type: Number,
+
         required: true
+
     },
+
     category: {
+
         type: String,
+
         required: true
+
     },
+
     image: {
+
         type: String,
+
         required: true
+
     }
+
 }, {
+
     timestamps: true
+
 });
 
-// Export the model only if it hasn't been compiled yet
-module.exports = mongoose.models.Product || mongoose.model('Product', productSchema); 
+
+
+// Export the model
+
+const Product = mongoose.models.Product || mongoose.model('Product', productSchema);
+
+module.exports = Product; 
